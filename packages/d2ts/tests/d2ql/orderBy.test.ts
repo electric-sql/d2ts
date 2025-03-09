b504ae30e6bc8d71c90818786085aff532ada81f
Sam Willis <sam.willis@gmail.com>
2025-03-09 15:30:49 +0000

    Tidy up tests

--- packages/d2ts/tests/d2ql/orderBy.test.ts
+++ packages/d2ts/tests/d2ql/orderBy.test.ts
@@ -50,331 +50,338 @@ describe('D2QL', () => {
       )
     })
 
-    test('initial results with orderBy', () => {
-      const query: Query = {
-        select: ['@id', '@value'],
-        from: 'input',
-        orderBy: '@value',
-      }
+    describe('with no index', () => {
+      test('initial results', () => {
+        const query: Query = {
+          select: ['@id', '@value'],
+          from: 'input',
+          orderBy: '@value',
+        }
 
-      const graph = new D2({ initialFrontier: 0 })
-      const input = graph.newInput<{
-        id: number
-        value: string
-      }>()
-      let latestMessage: any = null
+        const graph = new D2({ initialFrontier: 0 })
+        const input = graph.newInput<{
+          id: number
+          value: string
+        }>()
+        let latestMessage: any = null
 
-      const pipeline = compileQuery(query, { input })
-      pipeline.pipe(
-        output((message) => {
-          if (message.type === MessageType.DATA) {
-            latestMessage = message.data
-          }
-        }),
-      )
+        const pipeline = compileQuery(query, { input })
+        pipeline.pipe(
+          output((message) => {
+            if (message.type === MessageType.DATA) {
+              latestMessage = message.data
+            }
+          }),
+        )
 
-      graph.finalize()
+        graph.finalize()
 
-      input.sendData(
-        0,
-        new MultiSet([
+        input.sendData(
+          0,
+          new MultiSet([
+            [{ id: 1, value: 'a' }, 1],
+            [{ id: 2, value: 'z' }, 1],
+            [{ id: 3, value: 'b' }, 1],
+            [{ id: 4, value: 'y' }, 1],
+            [{ id: 5, value: 'c' }, 1],
+          ]),
+        )
+        input.sendFrontier(1)
+
+        graph.run()
+
+        expect(latestMessage).not.toBeNull()
+
+        const result = latestMessage.collection.getInner()
+
+        expect(
+          sortResults(result, (a, b) => a.value.localeCompare(b.value)),
+        ).toEqual([
           [{ id: 1, value: 'a' }, 1],
-          [{ id: 2, value: 'z' }, 1],
           [{ id: 3, value: 'b' }, 1],
-          [{ id: 4, value: 'y' }, 1],
           [{ id: 5, value: 'c' }, 1],
-        ]),
-      )
-      input.sendFrontier(1)
-
-      graph.run()
-
-      expect(latestMessage).not.toBeNull()
-
-      const result = latestMessage.collection.getInner()
-
-      expect(
-        sortResults(result, (a, b) => a.value.localeCompare(b.value)),
-      ).toEqual([
-        [{ id: 1, value: 'a' }, 1],
-        [{ id: 3, value: 'b' }, 1],
-        [{ id: 5, value: 'c' }, 1],
-        [{ id: 4, value: 'y' }, 1],
-        [{ id: 2, value: 'z' }, 1],
-      ])
+          [{ id: 4, value: 'y' }, 1],
+          [{ id: 2, value: 'z' }, 1],
+        ])
+      })
     })
+    describe('with no index and keyBy', () => {
+      test('initial results', () => {
+        const query: Query = {
+          select: ['@id', '@value'],
+          from: 'input',
+          keyBy: '@id',
+          orderBy: '@value',
+        }
 
-    test('initial results with a keyBy and orderBy', () => {
-      const query: Query = {
-        select: ['@id', '@value'],
-        from: 'input',
-        keyBy: '@id',
-        orderBy: '@value',
-      }
+        const graph = new D2({ initialFrontier: 0 })
+        const input = graph.newInput<{
+          id: number
+          value: string
+        }>()
+        let latestMessage: any = null
 
-      const graph = new D2({ initialFrontier: 0 })
-      const input = graph.newInput<{
-        id: number
-        value: string
-      }>()
-      let latestMessage: any = null
+        const pipeline = compileQuery(query, { input })
+        pipeline.pipe(
+          output((message) => {
+            if (message.type === MessageType.DATA) {
+              latestMessage = message.data
+            }
+          }),
+        )
 
-      const pipeline = compileQuery(query, { input })
-      pipeline.pipe(
-        output((message) => {
-          if (message.type === MessageType.DATA) {
-            latestMessage = message.data
-          }
-        }),
-      )
+        graph.finalize()
 
-      graph.finalize()
+        input.sendData(
+          0,
+          new MultiSet([
+            [{ id: 1, value: 'a' }, 1],
+            [{ id: 2, value: 'z' }, 1],
+            [{ id: 3, value: 'b' }, 1],
+            [{ id: 4, value: 'y' }, 1],
+            [{ id: 5, value: 'c' }, 1],
+          ]),
+        )
+        input.sendFrontier(1)
 
-      input.sendData(
-        0,
-        new MultiSet([
-          [{ id: 1, value: 'a' }, 1],
-          [{ id: 2, value: 'z' }, 1],
-          [{ id: 3, value: 'b' }, 1],
-          [{ id: 4, value: 'y' }, 1],
-          [{ id: 5, value: 'c' }, 1],
-        ]),
-      )
-      input.sendFrontier(1)
+        graph.run()
 
-      graph.run()
+        expect(latestMessage).not.toBeNull()
 
-      expect(latestMessage).not.toBeNull()
+        const result = latestMessage.collection.getInner()
 
-      const result = latestMessage.collection.getInner()
-
-      expect(
-        sortResults(result, (a, b) => a[1].value.localeCompare(b[1].value)),
-      ).toEqual([
-        [[1, { id: 1, value: 'a' }], 1],
-        [[3, { id: 3, value: 'b' }], 1],
-        [[5, { id: 5, value: 'c' }], 1],
-        [[4, { id: 4, value: 'y' }], 1],
-        [[2, { id: 2, value: 'z' }], 1],
-      ])
+        expect(
+          sortResults(result, (a, b) => a[1].value.localeCompare(b[1].value)),
+        ).toEqual([
+          [[1, { id: 1, value: 'a' }], 1],
+          [[3, { id: 3, value: 'b' }], 1],
+          [[5, { id: 5, value: 'c' }], 1],
+          [[4, { id: 4, value: 'y' }], 1],
+          [[2, { id: 2, value: 'z' }], 1],
+        ])
+      })
     })
+    describe('with numeric index', () => {
+      test('initial results', () => {
+        const query: Query = {
+          select: ['@id', '@value', { index: { ORDER_INDEX: 'numeric' } }],
+          from: 'input',
+          orderBy: '@value',
+        }
 
-    test('initial results with orderBy and numeric index', () => {
-      const query: Query = {
-        select: ['@id', '@value', { index: { ORDER_INDEX: 'numeric' } }],
-        from: 'input',
-        orderBy: '@value',
-      }
+        const graph = new D2({ initialFrontier: 0 })
+        const input = graph.newInput<{
+          id: number
+          value: string
+        }>()
+        let latestMessage: any = null
 
-      const graph = new D2({ initialFrontier: 0 })
-      const input = graph.newInput<{
-        id: number
-        value: string
-      }>()
-      let latestMessage: any = null
+        const pipeline = compileQuery(query, { input })
+        pipeline.pipe(
+          output((message) => {
+            if (message.type === MessageType.DATA) {
+              latestMessage = message.data
+            }
+          }),
+        )
 
-      const pipeline = compileQuery(query, { input })
-      pipeline.pipe(
-        output((message) => {
-          if (message.type === MessageType.DATA) {
-            latestMessage = message.data
-          }
-        }),
-      )
+        graph.finalize()
 
-      graph.finalize()
+        input.sendData(
+          0,
+          new MultiSet([
+            [{ id: 1, value: 'a' }, 1],
+            [{ id: 2, value: 'z' }, 1],
+            [{ id: 3, value: 'b' }, 1],
+            [{ id: 4, value: 'y' }, 1],
+            [{ id: 5, value: 'c' }, 1],
+          ]),
+        )
+        input.sendFrontier(1)
 
-      input.sendData(
-        0,
-        new MultiSet([
-          [{ id: 1, value: 'a' }, 1],
-          [{ id: 2, value: 'z' }, 1],
-          [{ id: 3, value: 'b' }, 1],
-          [{ id: 4, value: 'y' }, 1],
-          [{ id: 5, value: 'c' }, 1],
-        ]),
-      )
-      input.sendFrontier(1)
+        graph.run()
 
-      graph.run()
+        expect(latestMessage).not.toBeNull()
 
-      expect(latestMessage).not.toBeNull()
+        const result = latestMessage.collection.getInner()
 
-      const result = latestMessage.collection.getInner()
-
-      expect(
-        sortResults(result, (a, b) => a.value.localeCompare(b.value)),
-      ).toEqual([
-        [{ id: 1, value: 'a', index: 0 }, 1],
-        [{ id: 3, value: 'b', index: 1 }, 1],
-        [{ id: 5, value: 'c', index: 2 }, 1],
-        [{ id: 4, value: 'y', index: 3 }, 1],
-        [{ id: 2, value: 'z', index: 4 }, 1],
-      ])
+        expect(
+          sortResults(result, (a, b) => a.value.localeCompare(b.value)),
+        ).toEqual([
+          [{ id: 1, value: 'a', index: 0 }, 1],
+          [{ id: 3, value: 'b', index: 1 }, 1],
+          [{ id: 5, value: 'c', index: 2 }, 1],
+          [{ id: 4, value: 'y', index: 3 }, 1],
+          [{ id: 2, value: 'z', index: 4 }, 1],
+        ])
+      })
     })
+    describe('with keyBy and numeric index', () => {
+      test('initial results', () => {
+        const query: Query = {
+          select: ['@id', '@value', { index: { ORDER_INDEX: 'numeric' } }],
+          from: 'input',
+          keyBy: '@id',
+          orderBy: '@value',
+        }
 
-    test('initial results with a keyBy and orderBy and numeric index', () => {
-      const query: Query = {
-        select: ['@id', '@value', { index: { ORDER_INDEX: 'numeric' } }],
-        from: 'input',
-        keyBy: '@id',
-        orderBy: '@value',
-      }
+        const graph = new D2({ initialFrontier: 0 })
+        const input = graph.newInput<{
+          id: number
+          value: string
+        }>()
+        let latestMessage: any = null
 
-      const graph = new D2({ initialFrontier: 0 })
-      const input = graph.newInput<{
-        id: number
-        value: string
-      }>()
-      let latestMessage: any = null
+        const pipeline = compileQuery(query, { input })
+        pipeline.pipe(
+          output((message) => {
+            if (message.type === MessageType.DATA) {
+              latestMessage = message.data
+            }
+          }),
+        )
 
-      const pipeline = compileQuery(query, { input })
-      pipeline.pipe(
-        output((message) => {
-          if (message.type === MessageType.DATA) {
-            latestMessage = message.data
-          }
-        }),
-      )
+        graph.finalize()
 
-      graph.finalize()
+        input.sendData(
+          0,
+          new MultiSet([
+            [{ id: 1, value: 'a' }, 1],
+            [{ id: 2, value: 'z' }, 1],
+            [{ id: 3, value: 'b' }, 1],
+            [{ id: 4, value: 'y' }, 1],
+            [{ id: 5, value: 'c' }, 1],
+          ]),
+        )
+        input.sendFrontier(1)
 
-      input.sendData(
-        0,
-        new MultiSet([
-          [{ id: 1, value: 'a' }, 1],
-          [{ id: 2, value: 'z' }, 1],
-          [{ id: 3, value: 'b' }, 1],
-          [{ id: 4, value: 'y' }, 1],
-          [{ id: 5, value: 'c' }, 1],
-        ]),
-      )
-      input.sendFrontier(1)
+        graph.run()
 
-      graph.run()
+        expect(latestMessage).not.toBeNull()
 
-      expect(latestMessage).not.toBeNull()
+        const result = latestMessage.collection.getInner()
 
-      const result = latestMessage.collection.getInner()
-
-      expect(
-        sortResults(result, (a, b) => a[1].value.localeCompare(b[1].value)),
-      ).toEqual([
-        [[1, { id: 1, value: 'a', index: 0 }], 1],
-        [[3, { id: 3, value: 'b', index: 1 }], 1],
-        [[5, { id: 5, value: 'c', index: 2 }], 1],
-        [[4, { id: 4, value: 'y', index: 3 }], 1],
-        [[2, { id: 2, value: 'z', index: 4 }], 1],
-      ])
+        expect(
+          sortResults(result, (a, b) => a[1].value.localeCompare(b[1].value)),
+        ).toEqual([
+          [[1, { id: 1, value: 'a', index: 0 }], 1],
+          [[3, { id: 3, value: 'b', index: 1 }], 1],
+          [[5, { id: 5, value: 'c', index: 2 }], 1],
+          [[4, { id: 4, value: 'y', index: 3 }], 1],
+          [[2, { id: 2, value: 'z', index: 4 }], 1],
+        ])
+      })
     })
+    describe('with fractional index', () => {
+      test('initial results', () => {
+        const query: Query = {
+          select: ['@id', '@value', { index: { ORDER_INDEX: 'fractional' } }],
+          from: 'input',
+          orderBy: '@value',
+        }
 
-    test('initial results with orderBy and fractional index', () => {
-      const query: Query = {
-        select: ['@id', '@value', { index: { ORDER_INDEX: 'fractional' } }],
-        from: 'input',
-        orderBy: '@value',
-      }
+        const graph = new D2({ initialFrontier: 0 })
+        const input = graph.newInput<{
+          id: number
+          value: string
+        }>()
+        let latestMessage: any = null
 
-      const graph = new D2({ initialFrontier: 0 })
-      const input = graph.newInput<{
-        id: number
-        value: string
-      }>()
-      let latestMessage: any = null
+        const pipeline = compileQuery(query, { input })
+        pipeline.pipe(
+          output((message) => {
+            if (message.type === MessageType.DATA) {
+              latestMessage = message.data
+            }
+          }),
+        )
 
-      const pipeline = compileQuery(query, { input })
-      pipeline.pipe(
-        output((message) => {
-          if (message.type === MessageType.DATA) {
-            latestMessage = message.data
-          }
-        }),
-      )
+        graph.finalize()
 
-      graph.finalize()
+        input.sendData(
+          0,
+          new MultiSet([
+            [{ id: 1, value: 'a' }, 1],
+            [{ id: 2, value: 'z' }, 1],
+            [{ id: 3, value: 'b' }, 1],
+            [{ id: 4, value: 'y' }, 1],
+            [{ id: 5, value: 'c' }, 1],
+          ]),
+        )
+        input.sendFrontier(1)
 
-      input.sendData(
-        0,
-        new MultiSet([
-          [{ id: 1, value: 'a' }, 1],
-          [{ id: 2, value: 'z' }, 1],
-          [{ id: 3, value: 'b' }, 1],
-          [{ id: 4, value: 'y' }, 1],
-          [{ id: 5, value: 'c' }, 1],
-        ]),
-      )
-      input.sendFrontier(1)
+        graph.run()
 
-      graph.run()
+        expect(latestMessage).not.toBeNull()
 
-      expect(latestMessage).not.toBeNull()
+        const result = latestMessage.collection.getInner()
 
-      const result = latestMessage.collection.getInner()
-
-      expect(
-        sortResults(result, (a, b) => a.value.localeCompare(b.value)),
-      ).toEqual([
-        [{ id: 1, value: 'a', index: 'a0' }, 1],
-        [{ id: 3, value: 'b', index: 'a1' }, 1],
-        [{ id: 5, value: 'c', index: 'a2' }, 1],
-        [{ id: 4, value: 'y', index: 'a3' }, 1],
-        [{ id: 2, value: 'z', index: 'a4' }, 1],
-      ])
+        expect(
+          sortResults(result, (a, b) => a.value.localeCompare(b.value)),
+        ).toEqual([
+          [{ id: 1, value: 'a', index: 'a0' }, 1],
+          [{ id: 3, value: 'b', index: 'a1' }, 1],
+          [{ id: 5, value: 'c', index: 'a2' }, 1],
+          [{ id: 4, value: 'y', index: 'a3' }, 1],
+          [{ id: 2, value: 'z', index: 'a4' }, 1],
+        ])
+      })
     })
+    describe('with keyBy and fractional index', () => {
+      test('initial results', () => {
+        const query: Query = {
+          select: ['@id', '@value', { index: { ORDER_INDEX: 'fractional' } }],
+          from: 'input',
+          keyBy: '@id',
+          orderBy: '@value',
+        }
 
-    test('initial results with a keyBy and orderBy and fractional index', () => {
-      const query: Query = {
-        select: ['@id', '@value', { index: { ORDER_INDEX: 'fractional' } }],
-        from: 'input',
-        keyBy: '@id',
-        orderBy: '@value',
-      }
+        const graph = new D2({ initialFrontier: 0 })
+        const input = graph.newInput<{
+          id: number
+          value: string
+        }>()
+        let latestMessage: any = null
 
-      const graph = new D2({ initialFrontier: 0 })
-      const input = graph.newInput<{
-        id: number
-        value: string
-      }>()
-      let latestMessage: any = null
+        const pipeline = compileQuery(query, { input })
+        pipeline.pipe(
+          output((message) => {
+            if (message.type === MessageType.DATA) {
+              latestMessage = message.data
+            }
+          }),
+        )
 
-      const pipeline = compileQuery(query, { input })
-      pipeline.pipe(
-        output((message) => {
-          if (message.type === MessageType.DATA) {
-            latestMessage = message.data
-          }
-        }),
-      )
+        graph.finalize()
 
-      graph.finalize()
+        input.sendData(
+          0,
+          new MultiSet([
+            [{ id: 1, value: 'a' }, 1],
+            [{ id: 2, value: 'z' }, 1],
+            [{ id: 3, value: 'b' }, 1],
+            [{ id: 4, value: 'y' }, 1],
+            [{ id: 5, value: 'c' }, 1],
+          ]),
+        )
+        input.sendFrontier(1)
 
-      input.sendData(
-        0,
-        new MultiSet([
-          [{ id: 1, value: 'a' }, 1],
-          [{ id: 2, value: 'z' }, 1],
-          [{ id: 3, value: 'b' }, 1],
-          [{ id: 4, value: 'y' }, 1],
-          [{ id: 5, value: 'c' }, 1],
-        ]),
-      )
-      input.sendFrontier(1)
+        graph.run()
 
-      graph.run()
+        expect(latestMessage).not.toBeNull()
 
-      expect(latestMessage).not.toBeNull()
+        const result = latestMessage.collection.getInner()
 
-      const result = latestMessage.collection.getInner()
-
-      expect(
-        sortResults(result, (a, b) => a[1].value.localeCompare(b[1].value)),
-      ).toEqual([
-        [[1, { id: 1, value: 'a', index: 'a0' }], 1],
-        [[3, { id: 3, value: 'b', index: 'a1' }], 1],
-        [[5, { id: 5, value: 'c', index: 'a2' }], 1],
-        [[4, { id: 4, value: 'y', index: 'a3' }], 1],
-        [[2, { id: 2, value: 'z', index: 'a4' }], 1],
-      ])
+        expect(
+          sortResults(result, (a, b) => a[1].value.localeCompare(b[1].value)),
+        ).toEqual([
+          [[1, { id: 1, value: 'a', index: 'a0' }], 1],
+          [[3, { id: 3, value: 'b', index: 'a1' }], 1],
+          [[5, { id: 5, value: 'c', index: 'a2' }], 1],
+          [[4, { id: 4, value: 'y', index: 'a3' }], 1],
+          [[2, { id: 2, value: 'z', index: 'a4' }], 1],
+        ])
+      })
     })
   })
 })
