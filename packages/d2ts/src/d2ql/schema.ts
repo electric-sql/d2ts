d1008e4b9a692cbbe8175be7cb0d3799508bbb31
Sam Willis <sam.willis@gmail.com>
2025-04-27 17:02:51 +0100

    wip

--- packages/d2ts/src/d2ql/schema.ts
+++ packages/d2ts/src/d2ql/schema.ts
@@ -23,10 +23,29 @@ export type JSONLike =
   | { [key: string]: JSONLike }
 
 // LiteralValue supports common primitives, JS Date, or undefined.
-export type LiteralValue = string | number | boolean | Date | null | undefined
+// We exclude strings that start with "@" because they are property references.
+export type LiteralValue =
+  | (string & {})
+  | number
+  | boolean
+  | Date
+  | null
+  | undefined
 
-// A column reference may be a template literal starting with "@" or an explicit object.
-export type ColumnReference = `@${string}` | { col: string }
+// These versions are for use with methods on the query builder where we want to
+// ensure that the argument is a string that does not start with "@".
+// Can be combined with PropertyReference for validating references.
+export type SafeString<T extends string> = T extends `@${string}` ? never : T
+export type OptionalSafeString<T extends any> = T extends string
+  ? SafeString<T>
+  : never
+export type LiteralValueWithSafeString<T extends any> =
+  | (OptionalSafeString<T> & {})
+  | number
+  | boolean
+  | Date
+  | null
+  | undefined
 
 // To force a literal value (which may be arbitrary JSON or a Date), wrap it in an object with the "value" key.
 export interface ExplicitLiteral {
@@ -60,12 +79,15 @@ export type FunctionCall<C extends Context = Context> = {
  * - A function call (as defined above)
  * - An array of operands (for example, for "in" clauses)
  */
-export type ConditionOperand<C extends Context = Context> =
+export type ConditionOperand<
+  C extends Context = Context,
+  T extends any = any,
+> =
   | LiteralValue
-  | ColumnReference
+  | PropertyReference<C>
   | ExplicitLiteral
   | FunctionCall<C>
-  | ConditionOperand<C>[]
+  | ConditionOperand<C, T>[]
 
 // Allowed SQL comparators.
 export type Comparator =
@@ -86,34 +108,41 @@ export type Comparator =
 export type LogicalOperator = 'and' | 'or'
 
 // A simple condition is a tuple: [left operand, comparator, right operand].
-export type SimpleCondition<C extends Context = Context> = [
-  ConditionOperand<C>,
-  Comparator,
-  ConditionOperand<C>,
-]
+export type SimpleCondition<
+  C extends Context = Context,
+  T extends any = any,
+> = [ConditionOperand<C, T>, Comparator, ConditionOperand<C, T>]
 
 // A flat composite condition allows all elements to be at the same level:
 // [left1, op1, right1, 'and'/'or', left2, op2, right2, ...]
-export type FlatCompositeCondition<C extends Context = Context> = [
-  ConditionOperand<C>,
+export type FlatCompositeCondition<
+  C extends Context = Context,
+  T extends any = any,
+> = [
+  ConditionOperand<C, T>,
   Comparator,
-  ConditionOperand<C>,
-  ...(LogicalOperator | ConditionOperand<C> | Comparator)[],
+  ConditionOperand<C, T>,
+  ...(LogicalOperator | ConditionOperand<C, T> | Comparator)[],
 ]
 
 // A nested composite condition combines conditions with logical operators
 // The first element can be a SimpleCondition or FlatCompositeCondition
 // followed by logical operators and more conditions
-export type NestedCompositeCondition<C extends Context = Context> = [
-  SimpleCondition<C> | FlatCompositeCondition<C>,
-  ...(LogicalOperator | SimpleCondition<C> | FlatCompositeCondition<C>)[],
+export type NestedCompositeCondition<
+  C extends Context = Context,
+  T extends any = any,
+> = [
+  SimpleCondition<C, T> | FlatCompositeCondition<C, T>,
+  ...(LogicalOperator | SimpleCondition<C, T> | FlatCompositeCondition<C, T>)[],
 ]
 
 // A condition is either a simple condition or a composite condition (flat or nested).
-export type Condition<C extends Context = Context> =
-  | SimpleCondition<C>
-  | FlatCompositeCondition<C>
-  | NestedCompositeCondition<C>
+export type Condition<
+  C extends Context = Context,
+  T extends any = any,
+> = SimpleCondition<C, T>
+// | FlatCompositeCondition<C>
+// | NestedCompositeCondition<C>
 
 // A join clause includes a join type, the table to join, an optional alias,
 // an "on" condition, and an optional "where" clause specific to the join.
@@ -129,28 +158,29 @@ export interface JoinClause<C extends Context = Context> {
 // or an array of such items.
 export type OrderBy<C extends Context = Context> =
   | PropertyReferenceString<C>
-  | { [column in PropertyReferenceString<C>]: 'asc' | 'desc' }
+  | { [column in PropertyReferenceString<C>]?: 'asc' | 'desc' }
+  | Record<PropertyReferenceString<C>, 'asc' | 'desc'>
   | Array<
       | PropertyReferenceString<C>
-      | { [column in PropertyReferenceString<C>]: 'asc' | 'desc' }
+      | { [column in PropertyReferenceString<C>]?: 'asc' | 'desc' }
     >
 
 export type Select<C extends Context = Context> =
-  | PropertyReference<C>
+  | PropertyReferenceString<C>
   | { [alias: string]: PropertyReference<C> | FunctionCall<C> }
-  | WildcardReference<C>
+  | WildcardReferenceString<C>
 
 export type As<_C extends Context = Context> = string
 
 export type From<C extends Context = Context> = InputReference<C>
 
-export type Where<_C extends Context = Context> = Condition<_C>
+export type Where<C extends Context = Context> = Condition<C>
 
 export type GroupBy<C extends Context = Context> =
   | PropertyReference<C>
   | PropertyReference<C>[]
 
-export type Having<_C extends Context = Context> = Condition<_C>
+export type Having<C extends Context = Context> = Condition<C>
 
 export type Limit<_C extends Context = Context> = number
 
@@ -166,7 +196,6 @@ export interface BaseQuery<C extends Context = Context> {
   from: From<C>
   join?: JoinClause<C>[]
   where?: Condition<C>
-  // groupBy may be a single column name or an array of column names.
   groupBy?: GroupBy<C>
   having?: Condition<C>
   orderBy?: OrderBy<C>
@@ -176,7 +205,7 @@ export interface BaseQuery<C extends Context = Context> {
 
 // The top-level query interface.
 export interface Query<C extends Context = Context> extends BaseQuery<C> {
-  keyBy?: string | string[]
+  keyBy?: PropertyReference<C> | PropertyReference<C>[]
   with?: WithQuery<C>[]
 }
 
@@ -190,10 +219,10 @@ export interface WithQuery<C extends Context = Context> extends BaseQuery<C> {
 // A keyed query is a query that has a keyBy clause, and so the result is always
 // a keyed stream.
 export interface KeyedQuery<C extends Context = Context> extends Query<C> {
-  keyBy: string | string[]
+  keyBy: PropertyReference<C> | PropertyReference<C>[]
 }
 
-// ============
+// ====== Some tests during development: ======
 
 interface TestSchema extends Schema {
   users: {
@@ -246,6 +275,13 @@ const qDesc: Query<{
   default: 'comments'
 }> = {
   from: 'comments',
-  select: ['@id'],
-  orderBy: { '@userId': 'desc' },
+  select: [
+    '@id',
+    {
+      test: '@authorId',
+    },
+  ],
+  orderBy: { '@comments.userId': 'desc' },
+  groupBy: ['@authorId', { col: 'posts.title' }],
+  where: ['@comments.content', '!=', 1],
 }
