7c1096c68e13e5054e545e153f7cb91797dccc49
Sam Willis <sam.willis@gmail.com>
2025-06-16 20:07:07 +0100

    fix tests

--- packages/d2mini/tests/utils.test.ts
+++ packages/d2mini/tests/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { DefaultMap, WeakRefMap, hash } from '../src/utils.js'
+import { DefaultMap, hash } from '../src/utils.js'
 
 describe('DefaultMap', () => {
   it('should return default value for missing keys', () => {
@@ -26,55 +26,6 @@ describe('DefaultMap', () => {
     map.update('key', (value) => value * 2)
     expect(map.get('key')).toBe(2)
   })
-})
-
-describe('WeakRefMap', () => {
-  it('should return default value for missing keys', () => {
-    const map = new WeakRefMap<string, object>()
-    expect(map.get('missing')).toBeNull()
-  })
-
-  it('should store and retrieve values', () => {
-    const map = new WeakRefMap<string, object>()
-    const obj = { test: 'value' }
-    map.set('key', obj)
-    expect(map.get('key')).toBe(obj)
-  })
-
-  it('should accept initial entries', () => {
-    const map = new WeakRefMap<string, object>()
-    const obj = { test: 'value' }
-    map.set('key', obj)
-    expect(map.get('key')).toBe(obj)
-  })
-
-  // it('should cleanup dereferenced objects after garbage collection', async () => {
-  //   const map = new WeakRefMap<string, object>()
-
-  //   // Create object in a scope that will end
-  //   {
-  //     const obj = { test: 'value' }
-  //     map.set('key', obj)
-  //     expect(map.get('key')).toBe(obj)
-  //   }
-
-  //   // Force garbage collection if possible
-  //   // Note: This is environment-specific and may not work everywhere
-  //   // with node use --expose-gc to enable the `global.gc()` method
-  //   if (global.gc) {
-  //     // Run GC multiple times to ensure cleanup
-  //     for (let i = 0; i < 3; i++) {
-  //       global.gc()
-  //       // Give finalizers a chance to run
-  //       await new Promise(resolve => setTimeout(resolve, 0))
-  //     }
-
-  //     // Object should be gone now
-  //     expect(map.get('key')).toBeNull()
-  //   } else {
-  //     console.warn('Test skipped: garbage collection not exposed. Run Node.js with --expose-gc flag.')
-  //   }
-  // })
 })
 
 describe('hash', () => {
