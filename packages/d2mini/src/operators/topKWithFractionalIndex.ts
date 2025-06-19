4fef94802d927e18384e5a030d3f5107112d2076
Kevin De Porre <kevin@electric-sql.com>
2025-06-19 13:37:33 +0200

    Introduce a TopK data structure

--- packages/d2mini/src/operators/topKWithFractionalIndex.ts
+++ packages/d2mini/src/operators/topKWithFractionalIndex.ts
@@ -25,6 +25,147 @@ const getValue = <V>(indexedValue: IndexedValue<V>): V => indexedValue[0]
 const getIndex = <V>(indexedValue: IndexedValue<V>): FractionalIndex =>
   indexedValue[1]
 
+////////////
+
+type TopKChanges<V> = {
+  /** Indicates which element moves into the topK (if any) */
+  moveIn: IndexedValue<V> | null
+  /** Indicates which element moves out of the topK (if any) */
+  moveOut: IndexedValue<V> | null
+}
+
+interface TopK<V> {
+  insert(value: V): TopKChanges<V>
+  delete(value: V): TopKChanges<V>
+}
+
+/**
+ * Implementation of a topK data structure.
+ * Uses a sorted array internally to store the values and keeps a topK window over that array.
+ * Inserts and deletes are O(n) operations because worst case an element is inserted/deleted
+ * at the start of the array which causes all the elements to shift to the right/left.
+ */
+class TopKArray<V> implements TopK<V> {
+  #sortedValues: Array<IndexedValue<V>> = []
+  #comparator: (a: V, b: V) => number
+  #topKStart: number
+  #topKEnd: number
+
+  constructor(
+    offset: number,
+    limit: number,
+    comparator: (a: V, b: V) => number,
+  ) {
+    this.#topKStart = offset
+    this.#topKEnd = offset + limit
+    this.#comparator = comparator
+  }
+
+  insert(value: V): TopKChanges<V> {
+    let result: TopKChanges<V> = { moveIn: null, moveOut: null }
+
+    // Lookup insert position
+    const index = this.#findIndex(value)
+    // Generate fractional index based on the fractional indices of the elements before and after it
+    const indexBefore =
+      index === 0 ? null : getIndex(this.#sortedValues[index - 1])
+    const indexAfter =
+      index === this.#sortedValues.length
+        ? null
+        : getIndex(this.#sortedValues[index])
+    const fractionalIndex = generateKeyBetween(indexBefore, indexAfter)
+
+    // Insert the value at the correct position
+    const val = indexedValue(value, fractionalIndex)
+    // Splice is O(n) where n = all elements in the collection (i.e. n >= k) !
+    this.#sortedValues.splice(index, 0, val)
+
+    // Check if the topK changed
+    if (index < this.#topKEnd) {
+      // The inserted element is either before the top K or within the top K
+      // If it is before the top K then it moves the element that was right before the topK into the topK
+      // If it is within the top K then the inserted element moves into the top K
+      // In both cases the last element of the old top K now moves out of the top K
+      const moveInIndex = Math.max(index, this.#topKStart)
+      if (moveInIndex < this.#sortedValues.length) {
+        // We actually have a topK
+        // because in some cases there may not be enough elements in the array to reach the start of the topK
+        // e.g. [1, 2, 3] with K = 2 and offset = 3 does not have a topK
+        result.moveIn = this.#sortedValues[moveInIndex]
+
+        // We need to remove the element that falls out of the top K
+        // The element that falls out of the top K has shifted one to the right
+        // because of the element we inserted, so we find it at index topKEnd
+        if (this.#topKEnd < this.#sortedValues.length) {
+          result.moveOut = this.#sortedValues[this.#topKEnd]
+        }
+      }
+    }
+
+    return result
+  }
+
+  /**
+   * Deletes a value that may or may not be in the topK.
+   * IMPORTANT: this assumes that the value is present in the collection
+   *            if it's not the case it will remove the element
+   *            that is on the position where the provided `value` would be.
+   */
+  delete(value: V): TopKChanges<V> {
+    let result: TopKChanges<V> = { moveIn: null, moveOut: null }
+
+    // Lookup delete position
+    const index = this.#findIndex(value)
+    // Remove the value at that position
+    const [removedElem] = this.#sortedValues.splice(index, 1)
+
+    // Check if the topK changed
+    if (index < this.#topKEnd) {
+      // The removed element is either before the top K or within the top K
+      // If it is before the top K then the first element of the topK moves out of the topK
+      // If it is within the top K then the removed element moves out of the topK
+      result.moveOut = removedElem
+      if (index < this.#topKStart) {
+        // The removed element is before the topK
+        // so actually, the first element of the topK moves out of the topK
+        // and not the element that we removed
+        // The first element of the topK is now at index topKStart - 1
+        // since we removed an element before the topK
+        const moveOutIndex = this.#topKStart - 1
+        if (moveOutIndex < this.#sortedValues.length) {
+          result.moveOut = this.#sortedValues[moveOutIndex]
+        } else {
+          // No value is moving out of the topK
+          // because there are no elements in the topK
+          result.moveOut = null
+        }
+      }
+
+      // Since we removed an element that was before or in the topK
+      // the first element after the topK moved one position to the left
+      // and thus falls into the topK now
+      const moveInIndex = this.#topKEnd - 1
+      if (moveInIndex < this.#sortedValues.length) {
+        result.moveIn = this.#sortedValues[moveInIndex]
+      }
+    }
+
+    return result
+  }
+
+  // TODO: see if there is a way to refactor the code for insert and delete in the topK above
+  //       because they are very similar, one is shifting the topK window to the left and the other is shifting it to the right
+  //       so i have the feeling there is a common pattern here and we can implement both cases using that pattern
+
+  #findIndex(value: V): number {
+    return binarySearch(this.#sortedValues, indexedValue(value, ''), (a, b) =>
+      this.#comparator(getValue(a), getValue(b)),
+    )
+  }
+}
+
+///////////
+
 /**
  * Operator for fractional indexed topK operations
  * This operator maintains fractional indices for sorted elements
@@ -35,12 +176,12 @@ export class TopKWithFractionalIndexOperator<K, V1> extends UnaryOperator<
   [K, IndexedValue<V1>]
 > {
   #index = new Index<K, V1>()
-  #comparator: (a: V1, b: V1) => number
-  #limit: number
-  #offset: number
 
-  /** A map of keys to a sorted array of values for those keys */
-  #sortedValues: Map<K, Array<IndexedValue<V1>>> = new Map()
+  /**
+   * topK data structure that supports insertions and deletions
+   * and returns changes to the topK.
+   */
+  #topK: TopKArray<V1>
 
   constructor(
     id: number,
@@ -50,9 +191,9 @@ export class TopKWithFractionalIndexOperator<K, V1> extends UnaryOperator<
     options: TopKWithFractionalIndexOptions,
   ) {
     super(id, inputA, output)
-    this.#comparator = comparator
-    this.#limit = options.limit ?? Infinity
-    this.#offset = options.offset ?? 0
+    const limit = options.limit ?? Infinity
+    const offset = options.offset ?? 0
+    this.#topK = new TopKArray(offset, limit, comparator)
   }
 
   run(): void {
@@ -79,137 +220,30 @@ export class TopKWithFractionalIndexOperator<K, V1> extends UnaryOperator<
     this.#index.addValue(key, [value, multiplicity])
     const newMultiplicity = this.#index.getMultiplicity(key, value)
 
+    let res: TopKChanges<V1> = { moveIn: null, moveOut: null }
     if (oldMultiplicity <= 0 && newMultiplicity > 0) {
       // The value was invisible but should now be visible
       // Need to insert it into the array of sorted values
-      const index = this.insert(key, value)
-
-      // Now check if the top K changed
-      const topKStart = this.#offset
-      const topKEnd = this.#offset + this.#limit
-
-      if (index < topKEnd) {
-        // The inserted element is either before the top K or within the top K
-        // If it is before the top K then it moves the the element that was right before the topK into the topK
-        // If it is within the top K then the inserted element moves into the top K
-        // In both cases the last element of the old top K now moves out of the top K
-        const moveInIndex = Math.max(index, topKStart)
-        const sortedValues = this.#sortedValues.get(key) ?? []
-        if (moveInIndex < sortedValues.length) {
-          // We actually have a topK
-          // because in some cases there may not be enough elements in the array to reach the start of the topK
-          // e.g. [1, 2, 3] with K = 2 and offset = 3 does not have a topK
-          const moveInValue = sortedValues[moveInIndex]
-          // signal the move in with a +1 diff in the output
-          result.push([[key, moveInValue], 1])
-
-          // We need to remove the element that falls out of the top K
-          // The element that falls out of the top K has shifted one to the right
-          // because of the element we inserted, so we find it at index topKEnd
-          if (topKEnd < sortedValues.length) {
-            const valueOut = sortedValues[topKEnd]
-            result.push([[key, valueOut], -1])
-          }
-        }
-      }
+      res = this.#topK.insert(value)
     } else if (oldMultiplicity > 0 && newMultiplicity <= 0) {
       // The value was visible but should now be invisible
       // Need to remove it from the array of sorted values
-      const [removedElem, removedIndex] = this.remove(key, value)
-
-      // Now check if the top K changed
-      const topKStart = this.#offset
-      const topKEnd = this.#offset + this.#limit
-
-      if (removedIndex < topKEnd) {
-        // The removed element is either before the top K or within the top K
-        // If it is before the top K then the first element of the topK moves out of the topK
-        // If it is within the top K then the removed element moves out of the topK
-        let moveOutValue: IndexedValue<V1> | null = removedElem
-        const sortedValues = this.#sortedValues.get(key) ?? []
-        if (removedIndex < topKStart) {
-          // The removed element is before the topK
-          // so actually, the first element of the topK moves out of the topK
-          // and not the element that we removed
-          // The first element of the topK is now and topKStart - 1
-          // since we removed an element before the topK
-          const moveOutIndex = topKStart - 1
-          if (moveOutIndex < sortedValues.length) {
-            moveOutValue = sortedValues[moveOutIndex]
-          } else {
-            // No value is moving out of the topK
-            // because there are no elements in the topK
-            moveOutValue = null
-          }
-        }
-
-        if (moveOutValue) {
-          // Signal the move out with a -1 diff in the output index
-          result.push([[key, moveOutValue], -1])
-        }
-
-        // Since we removed an element that was before or in the topK
-        // the first element after the topK moved one position to the left
-        // and hence now falls into the topK
-        const moveInIndex = topKEnd - 1
-        if (moveInIndex < sortedValues.length) {
-          const moveInValue = sortedValues[moveInIndex]
-          result.push([[key, moveInValue], 1])
-        }
-      }
+      res = this.#topK.delete(value)
     } else {
       // The value was invisible and it remains invisible
       // or it was visible and remains visible
       // so it doesn't affect the topK
-      return
     }
-  }
 
-  // TODO: see if there is a way to refactor the code for insertions and removals in the topK above
-  //       because they are very similar, one is shifting the topK window to the left and the other is shifting it to the right
-  //       so i have the feeling there is a common pattern here and we can implement both cases
-  //       on top of that pattern
+    if (res.moveIn) {
+      result.push([[key, res.moveIn], 1])
+    }
 
-  /**
-   * Inserts a value at the correct position
-   * into the sorted array of values for the given key.
-   * Returns the index of the newly inserted value.
-   */
-  insert(key: K, value: V1): number {
-    // Lookup insert position
-    const sortedValues = this.#sortedValues.get(key) ?? []
-    const index = binarySearch(sortedValues, indexedValue(value, ''), (a, b) =>
-      this.#comparator(getValue(a), getValue(b)),
-    )
+    if (res.moveOut) {
+      result.push([[key, res.moveOut], -1])
+    }
 
-    // Generate fractional index based on the fractional indices of the elements before and after it
-    const indexBefore = index === 0 ? null : getIndex(sortedValues[index - 1])
-    const indexAfter =
-      index === sortedValues.length ? null : getIndex(sortedValues[index])
-    const fractionalIndex = generateKeyBetween(indexBefore, indexAfter)
-
-    // Insert the value at the correct position
-    const val = indexedValue(value, fractionalIndex)
-    sortedValues.splice(index, 0, val) // O(n) ...
-    this.#sortedValues.set(key, sortedValues)
-    return index
-  }
-
-  /**
-   * Removes a value from the sorted array of values for the given key.
-   * Returns the index of the removed value.
-   * IMPORTANT: this assumes that the value is present in the array
-   *            if it's not the case it will remove the element
-   *            that is on the position where the provided `value` would be.
-   */
-  remove(key: K, value: V1): [IndexedValue<V1>, number] {
-    const sortedValues = this.#sortedValues.get(key) ?? []
-    const index = binarySearch(sortedValues, indexedValue(value, ''), (a, b) =>
-      this.#comparator(getValue(a), getValue(b)),
-    )
-    const [removedElement] = sortedValues.splice(index, 1) // O(n) ...
-    this.#sortedValues.set(key, sortedValues)
-    return [removedElement, index]
+    return
   }
 }
 
